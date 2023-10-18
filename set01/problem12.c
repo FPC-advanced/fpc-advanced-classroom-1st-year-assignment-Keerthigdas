1c196dd947658a2ddb880621f73ab508892cad53
keerthigdas <keerthig.bsc23@rvu.edu.in>
2023-10-18 10:48:28 +0530

    committed 10/18/2023, 10:48:27 AM

--- set01/problem12.c
+++ set01/problem12.c
@@ -30,6 +30,46 @@ void output(int n, Complex c[n], Complex result);
 
 int main()
 {
-	
+	int n;
+	Complex a,b,result;
+	n=get_n();
+	Complex c[n];
+	result=add_n_complex(n,c);
+	output(n,c,result);
+	return 0;
 }
 
+int get_n()
+{
+	int n;
+	printf("Enter the size of array");
+}
+
+void input_n_complex(int n, Complex c[n])
+{
+	int i;
+	for (i=0; i<n; i++)
+	{
+	printf("Enter real no.");
+	scanf("%f",&c[i].real);
+	printf("Enter imaginary no.");
+	scanf("%f",&c[i].imaginary);
+	}
+}
+
+Complex add_n_complex(int n, Complex c[n])
+{
+	Complex x={0,0};
+	int i;
+	for (i=0; i<n; i++)
+	{
+		x.real=c[i].real+x.real;
+		x.imaginary=c[i].imaginary+x.imaginary;
+	}
+	return x;
+}
+
+void output(int n, Complex c[n], Complex result)
+{
+	printf("The sum of %d complex number is %f + %f \n", n, result.real, result.imaginary);
+}
