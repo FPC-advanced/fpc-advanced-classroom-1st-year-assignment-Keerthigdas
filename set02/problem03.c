016e670e370f1e8cd23f1338214becc4a65078a5
keerthigdas <keerthig.bsc23@rvu.edu.in>
2023-10-30 20:35:43 +0530

    committed 10/30/2023, 8:35:42 PM

--- set02/problem03.c
+++ set02/problem03.c
@@ -33,5 +33,34 @@ int input_number()
 int is_composite(int n)
 {
     int count=0;
-    
+    if (n<=1)
+    {
+        return 0;
+    }
+
+    int i;
+    for (i=2;  i<=n; i++)
+    {
+        if (n%i==0)
+        {
+            count=count+1;
+            if (count >2)
+            {
+                return 1;
+            }
+        }
+    }
+    return 0;
+}
+
+void output(int n, int result)
+{
+    if (result)
+    {
+        printf("%d is a composite number.",n);
+    }
+    else
+    {
+        printf("%d is not a composite number",n)
+    }
 }
