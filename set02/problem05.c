747346642a52f80de661e587e7dee2f53b3071d8
keerthigdas <keerthig.bsc23@rvu.edu.in>
2023-11-05 19:40:35 +0530

    committed 11/5/2023, 7:40:34 PM

--- set02/problem05.c
+++ set02/problem05.c
@@ -10,3 +10,11 @@ void output(int a, int b, int gcd);*/
 
 /* GCD (a, b) = (a × b)/ LCM (a, b)*/
 
+#include <stdio.h>
+
+int input();
+int find_gcd(int a, int b);
+void output(int a, int b, int gcd);
+
+int main()
+{}
