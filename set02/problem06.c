e292058f5e4a650f87b092cd40a855ae90553f6f
keerthigdas <keerthig.bsc23@rvu.edu.in>
2023-11-18 22:37:59 +0530

    committed 11/18/2023, 10:37:58 PM

---
+++ set02/problem06.c
@@ -0,0 +1,28 @@
+/*06. Write a program to reverse a string.
+
+***Function Declarations***
+```c
+void input_string(char *a);
+void str_reverse(char *str, char *rev_str);
+void output(char *a, char *reverse_a);
+```
+
+***Input***
+```
+hello
+```
+
+***Output***
+```
+olleh
+```*/
+
+#include <stdio.h>
+
+int main()
+{
+    for (int i=0; i<=10; i++)
+    {
+        printf("%d", i);
+    }
+}
